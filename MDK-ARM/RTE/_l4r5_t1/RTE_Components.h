921829713cb7f6de10754bd475f11231fe4539c4
zhanzr <zhanzr@foxmail.com>
2017-12-30 15:47:41 +0800

    First simple demo.

---
+++ MDK-ARM/RTE/_l4r5_t1/RTE_Components.h
@@ -0,0 +1,22 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'l4r5_t1' 
+ * Target:  'l4r5_t1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+
+#endif /* RTE_COMPONENTS_H */
---
+++ README.md
@@ -0,0 +1,3 @@
+ADC doesn't work properly.
+
+The Clock configuration in the CubeMX project is incorrect.
